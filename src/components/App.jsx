84f91b37297a3b95801c4738569f92a0828553a4
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-10-31 01:43:46 +0530

    Fruits, Cart, Favs Context Setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,58 @@
+import { createContext, useState } from "react";
 import { Route, Routes } from "react-router";
 import Header from "./Header";
 import Store from "./Store";
 import Home from "./Home";
 import Product from "./Product";
 import Cart from "./Cart";
+import { FRUITS } from "../utils/constants";
+
+export const FruitsContext = createContext(FRUITS);
+export const CartContext = createContext([
+  {
+    fruitId: 1,
+    count: 2,
+  },
+  {
+    fruitId: 4,
+    count: 3,
+  },
+]);
+export const FavContext = createContext([0, 2, 4, 7]);
 
 function App() {
-  return (
-    <div className="w-full bg-bg font-body text-white">
-      <Header />
+  const initialCart = [
+    {
+      fruitId: 1,
+      count: 2,
+    },
+    {
+      fruitId: 4,
+      count: 3,
+    },
+  ];
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/store/:slug" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </div>
+  const [fruits, setFruits] = useState(FRUITS);
+  const [cart, setCart] = useState(initialCart);
+  const [favs, setFavs] = useState([0, 2, 4, 7]);
+
+  return (
+    <FruitsContext.Provider value={{ fruits, setFruits }}>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <FavContext.Provider value={{ favs, setFavs }}>
+          <div className="w-full bg-bg font-body text-white">
+            <Header />
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/store" element={<Store />} />
+              <Route path="/store/:slug" element={<Product />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </div>
+        </FavContext.Provider>
+      </CartContext.Provider>
+    </FruitsContext.Provider>
   );
 }
 
